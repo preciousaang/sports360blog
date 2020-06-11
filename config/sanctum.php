f75fd2f7da7043d39a64c80169da41e27cb70c3c
Precious Ibeagi <preciousaang@gmail.com>
2020-06-11 21:18:43 +0100

    dsadf

--- config/sanctum.php
+++ config/sanctum.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost:8000,127.0.0.1:8000', 'bet360pro.herokuapp.com')),
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost:8000,127.0.0.1:8000')),
 
     /*
     |--------------------------------------------------------------------------
