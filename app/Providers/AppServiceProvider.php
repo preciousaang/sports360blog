d1f8cfd34bbad8f37a88c162fe1e61211783cfa2
Precious Ibeagi <preciousaang@gmail.com>
2020-06-11 20:49:20 +0100

    updafaf

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,11 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-<<<<<<< HEAD
+        //update schema length
         Schema::defaultStringLength(191);
-=======
-        Scheme::defaultStringLength(191);
->>>>>>> 7c54b3725a90cec2851f2fbe57cf5a49569ab48b
     }
 
     /**
