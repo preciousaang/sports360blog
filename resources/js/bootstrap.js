a141c859f942bae07856bcf634a43af00cd12eae
Precious Ibeagi <preciousaang@gmail.com>
2020-06-11 20:19:05 +0100

    Ok

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -24,7 +24,9 @@ window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios.defaults.headers.common['Accept'] = 'application/json';
 
-window.axios.defaults.baseURL = "http://localhost:8000/api";
+// window.axios.defaults.baseURL = "http://localhost:8000/api";
+
+window.axios.defaults.baseURL = "https://https://bet360pro.herokuapp.com//api";
 
 axios.defaults.withCredentials = true;
 
